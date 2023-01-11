dfbc2f1d3545ccc2cbdd78b538004f5d9cc28f6b
rhehdgus2 <107159240+EliotDavid@users.noreply.github.com>
2023-01-11 17:33:46 +0900

    javabasic

---
+++ javabasic/src/chapter02/entity/TestPage.java
@@ -0,0 +1,20 @@
+package chapter02.entity;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		
+		Developer dv = new Developer();
+		Developer dv = new Developer();
+		
+		
+		//System.out.println(Developer.class.getName().isEmpty());
+		//System.out.println(Developer.class.getName().);
+		/*
+		System.out.println(Developer.class.get());
+		System.out.println(Developer.class.getName());
+		System.out.println(Developer.class.getName());
+		*/
+		}
+
+}
